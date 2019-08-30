6098d48128d15be5c2efa9933b7c9ca24f2cf5f9
Camilo Torres - Analista De Inteligencia De Negocios <ctorrest@clinimed.local>
2019-08-30 11:58:15 -0500

    ok

--- src/EmailDelegate.java
+++ src/EmailDelegate.java
@@ -19,14 +19,13 @@ import javax.mail.internet.MimeMessage;
  */
 public class EmailDelegate {
 
-    private static final String SMTP_HOST_NAME = "smtp-relay.gmail.com"; //smtp URL
-    private static final int SMTP_HOST_PORT = 25; //port number
+    private static final String SMTP_HOST_NAME = "smtp-relay.gmail.com";//"smtp.gmail.com"; //smtp URL
+    private static final int SMTP_HOST_PORT = 25;//587; //port number
     public static final String EMAIL = "notificacion@clinicadelcountry.com";
     public static final String PASSWORD = "Cdc18092013";
 
     //  public static final String mails = "abenitez@clinicadelcountry.com";
     //nmacaya@clinicadelcountry.com,cgonzalez@clinicadelcountry.com,jospina@clinicadelcountry.com,cvelasco@clinicadelcountry.com,acorrea@clinicadelcountry.com,lgomez@clinicadelcountry.com,avelasquez@clinicadelcountry.com,//
-    
     public static final long TIEMPO_SEGUNDOS_VERIFICAR_HORA = 20;
 
     public static String sendMailViaGodaddy(final List<String> to, final String subject, final String text) {
@@ -65,24 +64,19 @@ public class EmailDelegate {
             props.setProperty("mail.transport.protocol", "smtp");
             props.setProperty("mail.host", SMTP_HOST_NAME);
             props.put("mail.smtp.starttls.enable", "true");
-            //  props.put("mail.smtp.ssl.trust", SMTP_HOST_NAME);
             props.put("mail.smtp.auth", "true");
             props.put("mail.debug", "true");
             props.put("mail.smtp.ehlo", "false");
             props.setProperty("mail.user", EMAIL);
             props.setProperty("mail.password", PASSWORD);
             props.put("mail.smtp.port", SMTP_HOST_PORT);
-       //     props.put("mail.smtp.socketFactory.port", SMTP_HOST_PORT);
-//props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//props.put("mail.smtp.socketFactory.fallback", "false");
-            // Session mailSession = Session.getDefaultInstance(props, null);
 
             Session mailSession = Session.getInstance(props,
                     new javax.mail.Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(EMAIL, PASSWORD);
-                        }
-                    });
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(EMAIL, PASSWORD);
+                }
+            });
 
             MimeMessage message = new MimeMessage(mailSession);
             message.setSentDate(new java.util.Date());
